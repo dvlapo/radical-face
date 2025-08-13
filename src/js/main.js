63be06d7f80c0cb940029739d3e88648eeb3ad3c
dvlapo <agbooladolapo24@gmail.com>
2025-08-13 22:57:36 +0100

    complete hero animation

--- src/js/main.js
+++ src/js/main.js
@@ -2,8 +2,12 @@ import { gsap } from "gsap";
 import { SplitText } from "gsap/SplitText";
 gsap.registerPlugin(SplitText);
 
-const splitText = new SplitText(".loader-text", {
+const loaderSplitText = new SplitText(".loader-text", {
     type: "words",
+});
+
+const mainHeadingSplitText = new SplitText(".g-main__heading", {
+    type: "chars",
 });
 
 const loaderTl = gsap.timeline({
@@ -12,15 +16,17 @@ const loaderTl = gsap.timeline({
     },
 });
 
+const latest = gsap.utils.toArray(".g-latest");
+
 loaderTl
-    .from(splitText.words, {
+    .from(loaderSplitText.words, {
         duration: 1,
         opacity: 0,
         y: 50,
         stagger: 0.15,
     })
     .fromTo(
-        ".loader-icon",
+        ".g-loader-icon",
         {
             duration: 1,
             opacity: 0,
@@ -32,35 +38,74 @@ loaderTl
         },
         "+=0.3"
     )
-    .to(".loader-icon", {
+    .to(".g-loader-icon", {
         duration: 3,
         rotation: 360,
         repeat: -1,
     })
-    .from(".loader-text-1", { duration: 0.5, x: 20 }, "-=3.5")
-    .from(".loader-text-2", { duration: 0.5, x: -20 }, "<")
-    .from(".main", {
-        duration: 1.5,
+    .from(".g-loader-text-1", { duration: 0.4, x: 20 }, "-=3.5")
+    .from(".g-loader-text-2", { duration: 0.4, x: -20 }, "<")
+    .from(".g-main", {
+        duration: 1,
         rotate: 90,
         opacity: 0.5,
         ease: "power4.out",
-        delay: 0.8,
+        delay: 1.5,
     })
     .from(
-        ".header",
+        ".g-hero-image",
+        {
+            duration: 1.5,
+            opacity: 0.7,
+            rotate: 80,
+            ease: "power4.out",
+        },
+        "<"
+    )
+    .from(
+        mainHeadingSplitText.chars,
+        {
+            y: -100,
+            opacity: 0,
+            stagger: 0.08,
+            delay: 0.2,
+        },
+        "<"
+    )
+    .from(
+        ".g-hero-text",
+        {
+            opacity: 0,
+            y: 50,
+        },
+        "-=1"
+    )
+    .from(
+        latest,
+        {
+            opacity: 0,
+            yPercent: 100,
+            stagger: 0.05,
+            duration: 1.5,
+            ease: "power4.out",
+        },
+        "<"
+    )
+    .from(
+        ".g-header",
         {
             duration: 1,
             yPercent: -100,
             ease: "power4.out",
         },
-        "-=0.8"
+        "<"
     )
-    .to("body", {
+    .to(".g-body", {
         css: {
             overflow: "auto",
         },
     })
-    .to(".loader", {
+    .to(".g-loader", {
         css: {
             display: "none",
         },
